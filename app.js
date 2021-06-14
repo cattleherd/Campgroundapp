85e1a16b008513a895d6c7f6f1c55b62a46c5b6d
cattleherd <radwan.hussein@protonmail.com>
2021-06-14 17:14:47 -0400

    initialized mongoose database, connected mongoose database to app.js, created basic schema in models folder, created a test route (/makecampground) to test db connection

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/campdb', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected')
+});
+
 const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -13,5 +30,11 @@ app.listen(3000, ()=>{
 
 app.get('/', (req,res)=>{
     res.render('home')
+})
+
+app.get('/makecampground', async (req,res)=>{
+    const camp = new Campground({title: 'my backyard'});
+    await camp.save();
+    res.send(camp)
 })
 
