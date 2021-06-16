5c276720fb9109f1b58c16088f654d100ebc3cfb
cattleherd <radwan.hussein@protonmail.com>
2021-06-15 23:45:38 -0400

    added a form which creates a new campground and saves to db

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ db.once('open', function(){
 });
 
 const app = express();
+app.use(express.urlencoded({ extended: true }))
 
 
 app.set('view engine', 'ejs');
@@ -35,3 +36,20 @@ app.get('/campgrounds', async (req, res) =>{
     const campgrounds = await Campground.find({}); //grabs campground collection (all)
     res.render('campgrounds/index', { campgrounds })
 })
+
+app.get('/campgrounds/new', (req, res)=>{
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+    
+})
+
+app.get('/campgrounds/:id', async (req, res) =>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+})
+
